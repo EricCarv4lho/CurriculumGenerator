8d754dc9a2d82b18790df104eb8af17b1c3f2fd8
EricCarv4lho <ericcarv4lho@gmail.com>
2025-11-05 20:10:52 -0300

    feat(curriculum): add curriculum entity

---
+++ CurriculumGenerator/Entities/Curriculum.cs
@@ -0,0 +1,20 @@
+﻿namespace CurriculumGenerator.Entities
+{
+    public class Curriculum
+    {
+        public string FullName { get; set; } = String.Empty;
+
+        public string ProfessionalTitle { get; set; } = String.Empty;
+        public string City { get; set; } = String.Empty;
+        public string State { get; set; } = String.Empty;
+        public string PhoneNumber { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string LinkedinLink { get; set; } = String.Empty;
+        public string GitHubLink { get; set; } = String.Empty;
+        public string Goal { get; set; } = String.Empty;
+        public string Highlights { get; set; } = String.Empty;
+        public string Experience { get; set; } = String.Empty;
+        public List<string> SkillSet { get; set; } = [];
+
+    }
+}
