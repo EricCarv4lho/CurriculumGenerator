34be9bd7b765b41e882c4a3bf9416bd5dfcf8c90
EricCarv4lho <ericcarv4lho@gmail.com>
2025-11-05 20:16:27 -0300

    feat(curriculum): add controller to generate curriculum via service

---
+++ CurriculumGenerator/Controllers/CurriculumController.cs
@@ -0,0 +1,25 @@
+﻿using CurriculumGenerator.Entities;
+using CurriculumGenerator.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurriculumGenerator.Controllers
+{
+    [ApiController]
+    [Route("api/curriculum")]
+    public class CurriculumController : Controller
+    {  
+        private readonly CurriculumService _curriculumService;
+
+        public CurriculumController(CurriculumService curriculumService)
+        {
+            _curriculumService = curriculumService;
+        }
+
+        [HttpPost("generate")]
+        public IActionResult GenerateCurriculum(Curriculum curriculum)
+        {  
+            var result = _curriculumService.GenerateCurriculum(curriculum);
+            return File(result, "application/pdf", "curriculum.pdf");
+        }
+    }
+}
