1d79be50710719a1d82f91392bd4411caa097f5e
EricCarv4lho <ericcarv4lho@gmail.com>
2025-11-08 17:49:53 -0300

    feat: add Experience entity

--- CurriculumGenerator/Entities/Curriculum.cs
+++ CurriculumGenerator/Entities/Curriculum.cs
@@ -9,11 +9,11 @@
         public string State { get; set; } = String.Empty;
         public string PhoneNumber { get; set; } = String.Empty;
         public string Email { get; set; } = String.Empty;
-        public string LinkedinLink { get; set; } = String.Empty;
-        public string GitHubLink { get; set; } = String.Empty;
+        public string? LinkedinLink { get; set; } = String.Empty;
+        public string? GitHubLink { get; set; } = String.Empty;
         public string CareerObjective { get; set; } = String.Empty;
         public string Highlights { get; set; } = String.Empty;
-        public string Experience { get; set; } = String.Empty;
+        public List<Experience> Experiences { get; set; } = [];
         public List<string> SkillSet { get; set; } = [];
 
     }
---
+++ CurriculumGenerator/Entities/Experience.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CurriculumGenerator.Entities
+{
+    public class Experience
+    {
+        
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        public string JobTitle { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime StartDate { get; set; } 
+
+        public DateTime? EndDate { get; set; } = null;
+
+        public string Description { get; set; } = string.Empty;
+
+       
+
+
+
+    }
+}
