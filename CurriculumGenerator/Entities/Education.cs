04a171415e6a056176b1b970eb63db2025542b3b
EricCarv4lho <ericcarv4lho@gmail.com>
2025-11-14 18:32:12 -0300

    feat(curriculum): add Education entity

---
+++ CurriculumGenerator/Entities/Education.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CurriculumGenerator.Entities
+{
+    public class Education
+    {
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; } = null;
+
+        public string Course { get; set; } = string.Empty;
+
+        public string InstitutionName { get; set; } = string.Empty;
+
+        
+    }
+}
