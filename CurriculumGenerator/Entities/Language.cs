00f408d84909643d9f07c5f3123e08b33ddac12f
EricCarv4lho <ericcarv4lho@gmail.com>
2025-11-14 18:44:42 -0300

    feat(curriculum): add Language entity

---
+++ CurriculumGenerator/Entities/Language.cs
@@ -0,0 +1,8 @@
+﻿namespace CurriculumGenerator.Entities
+{
+    public class Language
+    {
+        public string? Name { get; set; } = string.Empty;
+        public string? Proficiency { get; set; } = string.Empty;
+    }
+}
